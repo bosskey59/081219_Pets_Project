c47358b31414ad99a7766934803c6351640545b3
Enoch2k2 <enoch2k2@hotmail.com>
2019-10-29 08:55:03 -0500

    updated home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,7 @@ export class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <h1 className="center indigo-text text-darken-3">Welcome to Pet Shop Redux!</h1>
       </div>
     )
   }
